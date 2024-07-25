2f46a1f498b8fe4cfa317ca2ce98d6b473a45455
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-07-26 01:37:34 +0530

    .
    .

--- BubbleSort.js
+++ BubbleSort.js
@@ -20,3 +20,16 @@ console.log(arr);
 //   11, 12, 22, 25,
 //   34, 64, 90, 98
 // ]
+
+// Complexity Analysis of Bubble Sort:
+// Time Complexity: O(N2)
+// Auxiliary Space: O(1)
+
+// Advantages of Bubble Sort:
+// Bubble sort is easy to understand and implement.
+// It does not require any additional memory space.
+// It is a stable sorting algorithm, meaning that elements with the same key value maintain their relative order in the sorted output.
+
+// Disadvantages of Bubble Sort:
+// Bubble sort has a time complexity of O(N2) which makes it very slow for large data sets.
+// Bubble sort is a comparison-based sorting algorithm, which means that it requires a comparison operator to determine the relative order of elements in the input data set. It can limit the efficiency of the algorithm in certain cases.
